d6ca67e8eb1093a06a570404aeedd4db3e5fccea
Ruxandra Anghel <ruxiang05@gmail.com>
2019-02-04 19:30:08 +0000

    Modify addModel for better handling

--- src/controllers/agent.js
+++ src/controllers/agent.js
@@ -4,24 +4,23 @@ const addModel = (req, res) => {
   const { model } = req.body;
   const agent = req.user;
 
-  if (model && agent) {
-    User.findOne({ email: model })
-      .exec()
-      .then((foundModel) => {
-        User.findById(agent.id, '-password', (err, foundAgent) => {
-          if (err) return res.status(401).json({ error: 'Could not find agent' });
-          if (!foundAgent.agentInfo.models.indexOf(foundModel.id) === -1) {
-            foundAgent.agentInfo.models.push(foundModel.id);
-            foundAgent.save();
-          } else {
-            return res.status(401).json({ error: 'Model already linked' });
-          }
-          return res.status(200).json({ message: 'Model added', agent: foundAgent });
-        });
-      }).catch(() => res.status(500).json({ error: 'Model not in database' }));
-  } else {
-    return res.status(500).json({ error: 'Model or agent non-existent' });
-  }
+  if (!model) res.status(500).json({ error: 'Model not provided' });
+  if (!agent) res.status(500).json({ error: 'Agent not provided' });
+
+  User.findOne({ email: model })
+    .exec()
+    .then((foundModel) => {
+      User.findById(agent.id, '-password', (err, foundAgent) => {
+        if (err) return res.status(401).json({ error: 'Could not find agent' });
+        if (!foundAgent.agentInfo.models.indexOf(foundModel.id) === -1) {
+          foundAgent.agentInfo.models.push(foundModel.id);
+          foundAgent.save();
+        } else {
+          return res.status(401).json({ error: 'Model already linked' });
+        }
+        return res.status(200).json({ message: 'Model added', agent: foundAgent });
+      });
+    }).catch(() => res.status(500).json({ error: 'Model not in database' }));
 };
 
 const getModels = (req, res) => {
