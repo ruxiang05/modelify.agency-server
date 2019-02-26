8d3f15ca67d6be26819027540a36f4d19c2632c7
Ruxandra Anghel <ruxiang05@gmail.com>
2019-02-26 22:37:49 +0000

    Add job response endpoints

--- src/routes/job.js
+++ src/routes/job.js
@@ -7,6 +7,9 @@ const JobController = require('../controllers/job');
 router.get('/all', verifyToken, JobController.getJobs);
 router.post('/create', verifyToken, JobController.createJob);
 router.put('/update', verifyToken, JobController.updateJob);
+router.put('/accept', verifyToken, JobController.acceptJob);
+router.put('/complete', verifyToken, JobController.completeJob);
+router.put('/decline', verifyToken, JobController.declineJob);
 router.delete('/delete', verifyToken, JobController.deleteJob);
 
 module.exports = router;
