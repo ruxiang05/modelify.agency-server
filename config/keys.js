9bacd8e3a32d47892073e3e84b291d784a3310e8
Ruxandra Anghel <ruxiang05@gmail.com>
2019-04-01 14:28:20 +0100

    fix: prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./env/prod');
-} if((process.env.NODE_ENV === 'test')) {
+} else if (process.env.NODE_ENV === 'test') {
   module.exports = require('./env/test');
 } else {
   module.exports = require('./env/dev');
